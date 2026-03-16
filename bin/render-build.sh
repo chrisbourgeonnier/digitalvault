6743dd8e8eda17e73cf4b5f951a0fa1dde072166
Chris Bourgeonnier <139029314+chrisbourgeonnier@users.noreply.github.com>
2026-03-16 18:36:36 +1030

    Use db:prepare to migrate all databases including Solid Queue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,10 +5,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# Migrate primary database
-bundle exec rails db:migrate
-
-# Migrate secondary databases (Solid Cache, Solid Queue, Action Cable)
-bundle exec rails db:migrate:cache
-bundle exec rails db:migrate:queue
-bundle exec rails db:migrate:cable
+# Migrate all databases (primary + solid_queue, solid_cache, cable)
+bundle exec rails db:prepare
